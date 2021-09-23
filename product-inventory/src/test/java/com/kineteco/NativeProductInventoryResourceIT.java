2574866eeb8b848b62530927f7e3ea90216d5dea
Katia Aresti <karesti@redhat.com>
2021-09-23 12:35:01 +0200

    final 02_02 status after creating the Product Inventory microservice

---
+++ product-inventory/src/test/java/com/kineteco/NativeProductInventoryResourceIT.java
@@ -0,0 +1,9 @@
+package com.kineteco;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeProductInventoryResourceIT extends ProductInventoryResourceTest {
+
+    // Execute the same tests but in native mode.
+}
