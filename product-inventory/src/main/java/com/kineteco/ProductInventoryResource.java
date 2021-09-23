8c66527038a33bc8545ee9599c434461f47d5271
Katia Aresti <karesti@redhat.com>
2021-09-23 13:13:28 +0200

    rest by id

--- product-inventory/src/main/java/com/kineteco/ProductInventoryResource.java
+++ product-inventory/src/main/java/com/kineteco/ProductInventoryResource.java
@@ -1,9 +1,14 @@
 package com.kineteco;
 
+import com.kineteco.model.ProductAvailability;
+import com.kineteco.model.ProductInventory;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.math.BigDecimal;
 
 @Path("/products")
 public class ProductInventoryResource {
@@ -12,5 +17,18 @@ public class ProductInventoryResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello RESTEasy";
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{sku}")
+    public ProductInventory inventory(@PathParam("sku") String sku) {
+        ProductInventory productInventory = new ProductInventory();
+        productInventory.setSku("KE180");
+        productInventory.setName("K-Eco 180");
+        productInventory.setProductAvailability(ProductAvailability.IN_STOCK);
+        productInventory.setQuantity(12);
+        productInventory.setPrice(BigDecimal.valueOf(315.0));
+        return productInventory;
     }
 }
