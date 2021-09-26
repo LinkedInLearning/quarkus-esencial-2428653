e803d977bcf6b1d92994732d7f400031a740d686
Katia Aresti <karesti@redhat.com>
2021-09-26 22:57:56 +0200

    doc

--- README.md
+++ README.md
@@ -1,23 +1,6 @@
 # Quarkus esencial
-## 06_02 Comunicar servicios mediante un cliente REST con inyección de dependencias en Quarkus
+## 06_03 Test unitarios con WireMock y Quarkus
 
 
-* `./mvnw quarkus:add-extension -Dextensions="rest-client,rest-client-jackson"`
-* Arrancamos en modo desarrollo y cambiamos la version %dev.quarkus.http.port=8081 para no tener conflicto con product inventory  
-* Creamos la interfaz 'ProductInventoryService' anotada con @RegisterRestClient
-* Vamos a anotar la clase con @Path y crearemos el método
 
-```java
-   @GET
-   @Path("/{sku}/stock")
-   Integer getStock(@PathParam("sku") String sku);
-```
 
-* Podemos usar un API programática para esto mismo, pero vamos a utilizar la inyección de dependencias de CDI
-@Inject junto a @RestClient
-  
-* El error nos indica de configurar kineteco-product-inventory/mp-rest/url=http://localhost:8080
-
-Comprobamos que el endpoint de productos esta disponible con `http http://localhost:8080/products/`
-Probamos `http http://localhost:8081/sales/KE36Li/availability\?units\=300`
-Probamos `http http://localhost:8081/sales/KE36Li/availability\?units\=900`
---
+++ sales-service/src/test/java/com/kineteco/ProductInventoryWiremock.java
@@ -0,0 +1,37 @@
+package com.kineteco;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
+
+import java.util.Collections;
+import java.util.Map;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+
+public class ProductInventoryWiremock implements QuarkusTestResourceLifecycleManager {
+  private WireMockServer wireMockServer;
+
+  @Override
+  public Map<String, String> start() {
+    wireMockServer = new WireMockServer();
+    wireMockServer.start();
+
+    stubFor(get(urlEqualTo("/products/123/stock"))
+        .willReturn(aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withBody("42")
+        ));
+
+    return Collections.singletonMap("kineteco-product-inventory/mp-rest/url", wireMockServer.baseUrl());
+  }
+
+  @Override
+  public void stop() {
+    if (null != wireMockServer) {
+      wireMockServer.stop();
+    }
+  }
+}
