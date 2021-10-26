357da6dc0079b6729b33f98634c82f5724cd9d4f
Katia Aresti <karesti@redhat.com>
2021-10-26 18:43:49 +0200

    remove path param

--- product-inventory/src/main/java/com/kineteco/ProductInventoryResource.java
+++ product-inventory/src/main/java/com/kineteco/ProductInventoryResource.java
@@ -5,7 +5,6 @@ import com.kineteco.model.ProductInventory;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.math.BigDecimal;
@@ -21,8 +20,7 @@ public class ProductInventoryResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    @Path("/{sku}")
-    public ProductInventory inventory(@PathParam("sku") String sku) {
+    public ProductInventory inventory(String sku) {
         ProductInventory productInventory = new ProductInventory();
         productInventory.setSku(sku);
         productInventory.setName("K-Eco 180");
--- product-inventory/src/test/java/com/kineteco/ProductInventoryResourceTest.java
+++ product-inventory/src/test/java/com/kineteco/ProductInventoryResourceTest.java
@@ -22,7 +22,8 @@ public class ProductInventoryResourceTest {
 
     @Test
     public void inventoryEndpoint() {
-        ProductInventory productInventory = given().when().get("/products/{sku}", "KE180").then().statusCode(200).extract().body()
+        ProductInventory productInventory = given().when().get("/products/123")
+              .then().statusCode(200).extract().body()
               .as(ProductInventory.class);
         assertEquals("KE180", productInventory.getSku());
     }
