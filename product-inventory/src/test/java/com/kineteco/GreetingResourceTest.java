b55f8776fa63ad57f0f2f3b34491b5b86a8f2750
Katia Aresti <karesti@redhat.com>
2021-10-26 17:59:34 +0200

    hello

--- product-inventory/src/test/java/com/kineteco/GreetingResourceTest.java
+++ product-inventory/src/test/java/com/kineteco/GreetingResourceTest.java
@@ -12,7 +12,7 @@ public class GreetingResourceTest {
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/products")
+          .when().get("/hello")
           .then()
              .statusCode(200)
              .body(is("Hello RESTEasy"));
