0949750f23203d27874c12da3296a65b2579d8c2
Katia Aresti <karesti@redhat.com>
2021-10-26 16:51:14 +0200

    greeting

--- product-inventory/src/main/java/com/kineteco/GreetingResource.java
+++ product-inventory/src/main/java/com/kineteco/GreetingResource.java
@@ -6,7 +6,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/products")
-public class ProductInventoryResource {
+public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
