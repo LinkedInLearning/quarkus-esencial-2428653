45d42d67630ff748ed1a7c9502ac96a31a2c4318
Katia Aresti <karesti@redhat.com>
2021-10-26 17:52:34 +0200

    init

--- product-inventory/src/main/java/com/kineteco/GreetingResource.java
+++ product-inventory/src/main/java/com/kineteco/GreetingResource.java
@@ -1,11 +1,13 @@
 package com.kineteco;
 
+import com.kineteco.model.ProductAvailability;
 import com.kineteco.model.ProductInventory;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.math.BigDecimal;
 
 @Path("/hello")
 public class GreetingResource {
@@ -21,7 +23,11 @@ public class GreetingResource {
     @Path("/123")
     public ProductInventory inventory() {
         ProductInventory productInventory = new ProductInventory();
-        productInventory.setSku("123");
+        productInventory.setSku("KE180");
+        productInventory.setName("K-Eco 180");
+        productInventory.setProductAvailability(ProductAvailability.IN_STOCK);
+        productInventory.setQuantity(12);
+        productInventory.setPrice(BigDecimal.valueOf(315.0));
         return productInventory;
     }
 }
