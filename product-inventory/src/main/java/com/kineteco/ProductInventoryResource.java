7a353eff38367565b6bfc3bdd59473a7c58ed497
Katia Aresti <karesti@redhat.com>
2021-10-27 16:03:08 +0200

    doc

--- product-inventory/src/main/java/com/kineteco/ProductInventoryResource.java
+++ product-inventory/src/main/java/com/kineteco/ProductInventoryResource.java
@@ -9,23 +9,9 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.Map;
 
 @Path("/products")
 public class ProductInventoryResource {
-
-    private Map<String, ProductInventory> inventory = new HashMap();
-
-    public ProductInventoryResource() {
-        ProductInventory productInventory = new ProductInventory("KE180");
-        productInventory.setSku("KE180");
-        productInventory.setName("K-Eco 180");
-        productInventory.setProductAvailability(ProductAvailability.IN_STOCK);
-        productInventory.setQuantity(12);
-        productInventory.setPrice(BigDecimal.valueOf(315.0));
-        inventory.put("KE180", productInventory);
-    }
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
@@ -37,6 +23,12 @@ public class ProductInventoryResource {
     @Produces(MediaType.APPLICATION_JSON)
     @Path("KE180")
     public Response inventory() {
-        return Response.ok(inventory.get("KE180")).build();
+        ProductInventory productInventory = new ProductInventory("KE180");
+        productInventory.setSku("KE180");
+        productInventory.setName("K-Eco 180");
+        productInventory.setProductAvailability(ProductAvailability.IN_STOCK);
+        productInventory.setQuantity(12);
+        productInventory.setPrice(BigDecimal.valueOf(315.0));
+        return Response.ok(productInventory).build();
     }
 }
