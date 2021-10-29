580bcb82437f17240ef58186d4f310e876c6b4fb
Katia Aresti <karesti@redhat.com>
2021-10-29 15:30:19 +0200

    after

--- product-inventory/src/main/java/com/kineteco/validation/KinetecoExceptionMapper.java
+++ product-inventory/src/main/java/com/kineteco/validation/KinetecoExceptionMapper.java
@@ -1,4 +1,30 @@
 package com.kineteco.validation;
 
-public class KinetecoExceptionMapper {
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class KinetecoExceptionMapper implements ExceptionMapper<ConstraintViolationException> {
+   @Override
+   public Response toResponse(ConstraintViolationException e) {
+      return Response.status(Response.Status.BAD_REQUEST)
+            .type(MediaType.APPLICATION_JSON_TYPE)
+            .entity(errorMessage(e)).build();
+   }
+
+   private JsonObject errorMessage(ConstraintViolationException e) {
+      JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+
+      for (ConstraintViolation v : e.getConstraintViolations()) {
+         objectBuilder.add(v.getPropertyPath().toString(), v.getMessage());
+      }
+      return objectBuilder.build();
+   }
 }
