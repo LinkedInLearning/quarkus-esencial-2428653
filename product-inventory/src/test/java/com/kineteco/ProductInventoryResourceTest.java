f8bcd3743195dc78870949e20c1e355d5a9f6bb6
Katia Aresti <karesti@redhat.com>
2021-10-30 16:39:44 +0200

    init

--- product-inventory/src/test/java/com/kineteco/ProductInventoryResourceTest.java
+++ product-inventory/src/test/java/com/kineteco/ProductInventoryResourceTest.java
@@ -44,7 +44,7 @@ public class ProductInventoryResourceTest {
               .get("/products")
               .then()
               .statusCode(Response.Status.OK.getStatusCode())
-              .body("$.size()", is(52));
+              .body("$.size()", is(53));
     }
 
     @Test
