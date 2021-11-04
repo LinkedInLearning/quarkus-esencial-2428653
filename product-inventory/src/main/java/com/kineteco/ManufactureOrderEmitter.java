d1d9ac487efbaeaf7c846acd086c928c20768462
Katia Aresti <karesti@redhat.com>
2021-11-04 23:14:24 +0100

    init

--- product-inventory/src/main/java/com/kineteco/ManufactureOrderEmitter.java
+++ product-inventory/src/main/java/com/kineteco/ManufactureOrderEmitter.java
@@ -11,14 +11,10 @@ import javax.enterprise.context.ApplicationScoped;
 public class ManufactureOrderEmitter {
    private static final Logger LOGGER = Logger.getLogger(ManufactureOrderEmitter.class);
 
-   @Channel("orders")
-   Emitter<ManufactureOrder> emitter;
-
    public void sendManufactureOrder(String sku, int quantity) {
       LOGGER.debugf("Emit manufacturing message for sku %s with %d quantity", sku, quantity);
       ManufactureOrder manufactureOrder = new ManufactureOrder();
       manufactureOrder.sku = sku;
       manufactureOrder.quantity = quantity;
-      emitter.send(manufactureOrder);
    }
 }
