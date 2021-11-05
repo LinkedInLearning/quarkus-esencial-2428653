c350ced3f484e74d7ffee94671af8c29945bddd5
Katia Aresti <karesti@redhat.com>
2021-11-05 16:22:33 +0100

    app scoped

--- product-inventory/src/main/java/com/kineteco/ProductInventoryResource.java
+++ product-inventory/src/main/java/com/kineteco/ProductInventoryResource.java
@@ -17,6 +17,7 @@ import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.jboss.logging.Logger;
 import org.jboss.resteasy.reactive.RestQuery;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.validation.Valid;
 import javax.validation.groups.ConvertGroup;
@@ -38,6 +39,7 @@ import java.util.List;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
 
+@ApplicationScoped
 @Path("/products")
 public class ProductInventoryResource {
     private static final Logger LOGGER = Logger.getLogger(ProductInventoryResource.class);
