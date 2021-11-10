bea5663282ea63fcffa2d78aac598c1f30b6598f
Katia Aresti <karesti@redhat.com>
2021-11-10 14:15:32 +0100

    init

--- product-inventory/src/main/java/com/kineteco/health/PingProductInventoryResourceHealthCheck.java
+++ product-inventory/src/main/java/com/kineteco/health/PingProductInventoryResourceHealthCheck.java
@@ -1,26 +1,5 @@
 package com.kineteco.health;
 
-import com.kineteco.ProductInventoryResource;
-import org.eclipse.microprofile.health.HealthCheck;
-import org.eclipse.microprofile.health.HealthCheckResponse;
-import org.eclipse.microprofile.health.Liveness;
+public class PingProductInventoryResourceHealthCheck {
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
-
-@ApplicationScoped
-@Liveness
-public class PingProductInventoryResourceHealthCheck implements HealthCheck {
-   @Inject
-   ProductInventoryResource productInventoryResource;
-
-   @Override
-   public HealthCheckResponse call() {
-      String response = productInventoryResource.health();
-      return HealthCheckResponse
-            .named("Ping Product Inventory Service")
-            .withData("Response", response)
-            .up()
-            .build();
-   }
 }
--- sales-service/src/main/java/com/kineteco/health/ProductInventoryResourceHealthCheck.java
+++ sales-service/src/main/java/com/kineteco/health/ProductInventoryResourceHealthCheck.java
@@ -1,40 +1,5 @@
 package com.kineteco.health;
 
-import com.kineteco.client.ProductInventoryServiceClient;
-import org.eclipse.microprofile.health.HealthCheck;
-import org.eclipse.microprofile.health.HealthCheckResponse;
-import org.eclipse.microprofile.health.Readiness;
-import org.eclipse.microprofile.rest.client.inject.RestClient;
+public class ProductInventoryResourceHealthCheck {
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
-import javax.ws.rs.WebApplicationException;
-
-@Readiness
-@ApplicationScoped
-public class ProductInventoryResourceHealthCheck implements HealthCheck {
-   @Inject
-   @RestClient
-   ProductInventoryServiceClient productInventoryServiceClient;
-
-   @Override
-   public HealthCheckResponse call() {
-      long size = 0;
-
-      try {
-         size = productInventoryServiceClient.size();
-      } catch (WebApplicationException ex) {
-         if (ex.getResponse().getStatus() >= 500) {
-            return HealthCheckResponse.named("ProductInventoryServiceCheck")
-                  .withData("exception", ex.getMessage())
-                  .down()
-                  .build();
-         }
-      }
-      return HealthCheckResponse
-            .named("ProductInventoryServiceCheck")
-            .withData("size", size)
-            .up()
-            .build();
-   }
 }
