cdbdcd8344301f5c2e860aa78cc5368a90e5051b
Katia Aresti <karesti@redhat.com>
2021-11-10 23:34:25 +0100

    solution

--- sales-service/src/main/java/com/kineteco/health/PingSalesResourceHealthCheck.java
+++ sales-service/src/main/java/com/kineteco/health/PingSalesResourceHealthCheck.java
@@ -1,5 +1,26 @@
 package com.kineteco.health;
 
-public class PingSalesResourceHealthCheck {
+import com.kineteco.SalesResource;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+@Liveness
+@ApplicationScoped
+public class PingSalesResourceHealthCheck implements HealthCheck {
+
+   @Inject
+   SalesResource salesResource;
+
+   @Override
+   public HealthCheckResponse call() {
+      String response = salesResource.health();
+      return HealthCheckResponse.named("Ping Sales Service")
+            .withData("Response", response)
+            .up()
+            .build();
+   }
 }
--- sales-service/src/main/java/com/kineteco/health/ProductInventoryResourceHealthCheck.java
+++ sales-service/src/main/java/com/kineteco/health/ProductInventoryResourceHealthCheck.java
@@ -1,17 +1,41 @@
 package com.kineteco.health;
 
+import com.kineteco.client.ProductInventoryServiceClient;
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Readiness;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.WebApplicationException;
 
+@Readiness
 @ApplicationScoped
 public class ProductInventoryResourceHealthCheck implements HealthCheck {
 
+   @Inject
+   @RestClient
+   ProductInventoryServiceClient productInventoryServiceClient;
+
    @Override
    public HealthCheckResponse call() {
+      long size = 0;
+
+      try {
+         size = productInventoryServiceClient.size();
+      } catch (WebApplicationException ex) {
+         if (ex.getResponse().getStatus() >= 500) {
+            return HealthCheckResponse
+                  .named("ProductInventoryCheck")
+                  .withData("err", ex.getMessage())
+                  .down()
+                  .build();
+         }
+      }
       return HealthCheckResponse
             .named("ProductInventoryCheck")
+            .withData("size", size)
             .up()
             .build();
    }
