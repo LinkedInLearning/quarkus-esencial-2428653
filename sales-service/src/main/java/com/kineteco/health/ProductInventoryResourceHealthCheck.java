e32e08d882155df27b14e2c82f8226d0e5c7ea49
Katia Aresti <karesti@redhat.com>
2021-11-10 23:04:27 +0100

    init

--- sales-service/src/main/java/com/kineteco/health/ProductInventoryResourceHealthCheck.java
+++ sales-service/src/main/java/com/kineteco/health/ProductInventoryResourceHealthCheck.java
@@ -1,5 +1,18 @@
 package com.kineteco.health;
 
-public class ProductInventoryResourceHealthCheck {
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
 
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class ProductInventoryResourceHealthCheck implements HealthCheck {
+
+   @Override
+   public HealthCheckResponse call() {
+      return HealthCheckResponse
+            .named("ProductInventoryCheck")
+            .up()
+            .build();
+   }
 }
